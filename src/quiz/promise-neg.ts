bb50e50062d3e8b121ec2d21b53986840b825858
WoweeZowee94 <forrestgraymusic@gmail.com>
2024-10-19 05:05:59 -0400

    completed quiz problems

--- src/quiz/promise-neg.ts
+++ src/quiz/promise-neg.ts
@@ -3,3 +3,26 @@ const array2D_3 = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function checkNegRow(nums: number[]): Promise<string> {
+    return new Promise((resolve, reject) => {
+        if (nums.length === 0) {
+            reject("Empty array is an invalid input...");
+        }
+        nums.forEach((num) => {
+            if (num < 0) {
+                console.log('Row contains negative number: ', nums);
+            }
+        });
+    });
+}
+
+function checkNeg2dArray(arr: number[][]): Promise<string[]> {
+    const promises = arr.map(row => checkNegRow(row));
+
+    return Promise.all(promises)
+}
+
+checkNeg2dArray(array2D_3).then(totalSum => {
+    console.log(totalSum);
+});
--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -3,3 +3,32 @@ const array2D_1 = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumRow(nums: number[]): Promise<number> {
+    return new Promise((resolve, reject) => {
+        if (nums.length === 0) {
+            reject("Empty array is an invalid input...");
+        }
+        let sum = 0;
+        nums.forEach((num) => {
+            sum += num;
+        });
+        resolve(sum);
+    });
+}
+
+function sum2dArray(arr: number[][]): Promise<number> {
+    const promises = arr.map(row => sumRow(row));
+
+    return Promise.all(promises).then((rowSums) => {
+        let sum = 0;
+        rowSums.forEach(row => {
+            sum += row;
+        })
+        return sum;
+    });
+}
+
+sum2dArray(array2D_1).then(totalSum => {
+    console.log(totalSum);
+});
